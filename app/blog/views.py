80c39b102d0c3ebd65f49e7304ca309847dfa2ae
SudharsunRavi <sudharsunr2004@gmail.com>
2025-09-27 11:52:52 +0530

    attached auth for blog

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from .models import Blog
 from .serializers import BlogSerializer
 
@@ -6,8 +9,12 @@ from .serializers import BlogSerializer
 class BlogListCreate(generics.ListCreateAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
 
 # GET (single with id), PUT/PATCH, DELETE
 class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
